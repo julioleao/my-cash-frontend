2a3e939f7f5b41e949c8d4acbc986b562bb6c546
julioleao <julio0023@live.com>
2020-10-11 14:51:17 -0300

    Alterada as URLs do Backend

---
+++ build/precache-manifest.b4ed43959b0cd62dd164663d55d3acca.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78823efd9c948ce8fecf6eae1aa16e18",
+    "url": "/index.html"
+  },
+  {
+    "revision": "396f5e76fd83f7857140",
+    "url": "/static/css/2.12f50c95.chunk.css"
+  },
+  {
+    "revision": "1f3ee1641819394e0851",
+    "url": "/static/css/main.5bec5334.chunk.css"
+  },
+  {
+    "revision": "396f5e76fd83f7857140",
+    "url": "/static/js/2.79cc1f33.chunk.js"
+  },
+  {
+    "revision": "dc24c5d5e6b5318864ea4eaf7476bf3e",
+    "url": "/static/js/2.79cc1f33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f3ee1641819394e0851",
+    "url": "/static/js/main.20c69b92.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "7799dece2c79854f63f09e7dfa528b88",
+    "url": "/static/media/boxed-bg.7799dece.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
